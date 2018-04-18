635fb671ea53774d9f6ec5fccafdfc855d2497d5
Valter Minute <valter.minute@gmail.com>
2018-04-18 21:59:11 +0200

    Initial commit

---
+++ RootCertificates.cpp
@@ -0,0 +1,130 @@
+#include <Arduino.h>
+#include <RootCertificates.h>
+
+// structure used to store domain/certificate pair
+typedef struct
+{
+  const char* domain;
+  const char* certificate;
+} ROOT_CERTIFICATE;
+
+// defines used to enable/disable specific domains (may be useful to reduce code size)
+#define GOOGLE_COM
+#define GOOGLEUSERCONTENT_COM
+
+// this is a table with a list of root certificates where a domain matches a certificate
+static ROOT_CERTIFICATE  certificates[]={
+#ifdef GOOGLE_COM
+  {
+    "google.com",    
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIDVDCCAjygAwIBAgIDAjRWMA0GCSqGSIb3DQEBBQUAMEIxCzAJBgNVBAYTAlVT\n"
+    "MRYwFAYDVQQKEw1HZW9UcnVzdCBJbmMuMRswGQYDVQQDExJHZW9UcnVzdCBHbG9i\n"
+    "YWwgQ0EwHhcNMDIwNTIxMDQwMDAwWhcNMjIwNTIxMDQwMDAwWjBCMQswCQYDVQQG\n"
+    "EwJVUzEWMBQGA1UEChMNR2VvVHJ1c3QgSW5jLjEbMBkGA1UEAxMSR2VvVHJ1c3Qg\n"
+    "R2xvYmFsIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2swYYzD9\n"
+    "9BcjGlZ+W988bDjkcbd4kdS8odhM+KhDtgPpTSEHCIjaWC9mOSm9BXiLnTjoBbdq\n"
+    "fnGk5sRgprDvgOSJKA+eJdbtg/OtppHHmMlCGDUUna2YRpIuT8rxh0PBFpVXLVDv\n"
+    "iS2Aelet8u5fa9IAjbkU+BQVNdnARqN7csiRv8lVK83Qlz6cJmTM386DGXHKTubU\n"
+    "1XupGc1V3sjs0l44U+VcT4wt/lAjNvxm5suOpDkZALeVAjmRCw7+OC7RHQWa9k0+\n"
+    "bw8HHa8sHo9gOeL6NlMTOdReJivbPagUvTLrGAMoUgRx5aszPeE4uwc2hGKceeoW\n"
+    "MPRfwCvocWvk+QIDAQABo1MwUTAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTA\n"
+    "ephojYn7qwVkDBF9qn1luMrMTjAfBgNVHSMEGDAWgBTAephojYn7qwVkDBF9qn1l\n"
+    "uMrMTjANBgkqhkiG9w0BAQUFAAOCAQEANeMpauUvXVSOKVCUn5kaFOSPeCpilKIn\n"
+    "Z57QzxpeR+nBsqTP3UEaBU6bS+5Kb1VSsyShNwrrZHYqLizz/Tt1kL/6cdjHPTfS\n"
+    "tQWVYrmm3ok9Nns4d0iXrKYgjy6myQzCsplFAMfOEVEiIuCl6rYVSAlk6l5PdPcF\n"
+    "PseKUgzbFbS9bZvlxrFUaKnjaZC2mqUPuLk/IH2uSrW4nOQdtqvmlKXBx4Ot2/Un\n"
+    "hw4EbNX/3aBd7YdStysVAq45pmp06drE57xNNB6pXE0zX5IJL4hmXXeXxx12E6nV\n"
+    "5fEWCRE11azbJHFwLJhWC9kXtNHjUStedejV0NxPNO3CBWaAocvmMw==\n"
+    "-----END CERTIFICATE-----\n"
+  },
+#endif
+#ifdef GOOGLEUSERCONTENT_COM
+  {
+    "googleusercontent.com",
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIDujCCAqKgAwIBAgILBAAAAAABD4Ym5g0wDQYJKoZIhvcNAQEFBQAwTDEgMB4G\n"
+    "A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjIxEzARBgNVBAoTCkdsb2JhbFNp\n"
+    "Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDYxMjE1MDgwMDAwWhcNMjExMjE1\n"
+    "MDgwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEG\n"
+    "A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n"
+    "hvcNAQEBBQADggEPADCCAQoCggEBAKbPJA6+Lm8omUVCxKs+IVSbC9N/hHD6ErPL\n"
+    "v4dfxn+G07IwXNb9rfF73OX4YJYJkhD10FPe+3t+c4isUoh7SqbKSaZeqKeMWhG8\n"
+    "eoLrvozps6yWJQeXSpkqBy+0Hne/ig+1AnwblrjFuTosvNYSuetZfeLQBoZfXklq\n"
+    "tTleiDTsvHgMCJiEbKjNS7SgfQx5TfC4LcshytVsW33hoCmEofnTlEnLJGKRILzd\n"
+    "C9XZzPnqJworc5HGnRusyMvo4KD0L5CLTfuwNhv2GXqF4G3yYROIXJ/gkwpRl4pa\n"
+    "zq+r1feqCapgvdzZX99yqWATXgAByUr6P6TqBwMhAo6CygPCm48CAwEAAaOBnDCB\n"
+    "mTAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUm+IH\n"
+    "V2ccHsBqBt5ZtJot39wZhi4wNgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2NybC5n\n"
+    "bG9iYWxzaWduLm5ldC9yb290LXIyLmNybDAfBgNVHSMEGDAWgBSb4gdXZxwewGoG\n"
+    "3lm0mi3f3BmGLjANBgkqhkiG9w0BAQUFAAOCAQEAmYFThxxol4aR7OBKuEQLq4Gs\n"
+    "J0/WwbgcQ3izDJr86iw8bmEbTUsp9Z8FHSbBuOmDAGJFtqkIk7mpM0sYmsL4h4hO\n"
+    "291xNBrBVNpGP+DTKqttVCL1OmLNIG+6KYnX3ZHu01yiPqFbQfXf5WRDLenVOavS\n"
+    "ot+3i9DAgBkcRcAtjOj4LaR0VknFBbVPFd5uRHg5h6h+u/N5GJG79G+dwfCMNYxd\n"
+    "AfvDbbnvRG15RjF+Cv6pgsH/76tuIMRQyV+dTZsXjAzlAcmgQWpzU/qlULRuJQ/7\n"
+    "TBj0/VLZjmmx6BEP3ojY+x1J96relc8geMJgEtslQIxq/H5COEBkEveegeGTLg==\n"
+    "-----END CERTIFICATE-----\n"  
+  },
+#endif
+  { NULL, NULL }
+};
+
+const char* getCertificate(const char* url)
+{
+  char  domain[255];
+  char  *p,*q;
+  
+  // extract domain from URL
+  
+  // scheme
+  p=strstr(url,"//");
+  
+  // invalid URL
+  if (p==NULL)
+    return NULL;
+    
+  // skip scheme id  
+  p+=2;
+  
+  for (q=p;*q!=0;q++)
+  {
+    // url included username/pass, we can skip that
+    if (*q=='@')
+      p=q+1;
+    
+    // end of domain
+    if (*q=='/')
+       break;      
+  }
+  
+  if (q-p>253)
+    return NULL;
+        
+  memcpy(domain,p,(q-p));
+  domain[q-p]=0;
+  
+  strlwr(domain);
+  p=domain;   
+  
+  while (*p)
+  {
+    ROOT_CERTIFICATE* c;
+    
+    Serial.print("Searching certificate for domain:");
+    Serial.println(p);
+        
+    for (c=certificates;c->domain!=NULL;c++)
+    {
+      if (strcmp(p,c->domain)==0)
+        return c->certificate;
+    }
+    
+    p=strstr(p,".");
+    
+    if(!p)
+      break;
+      
+    p++;
+  }
+  return NULL;
+}
---
+++ RootCertificates.h
@@ -0,0 +1,6 @@
+#ifndef ROOTCERTIFICATE_H_
+#define ROOTCERTIFICATE_H_
+
+const char* getCertificate(const char* url);
+
+#endif //ROOTCERTIFICATE_H_
